ddb4f9e8c6ff8557cbccc0d14b9123a170b19837
Marcos Scheeren <marcosscheeren@gmail.com>
2020-12-20 06:39:17 -0800

    remove more mbgl qt stuff

--- main.cpp
+++ main.cpp
@@ -3,19 +3,12 @@
 #include <QCommandLineOption>
 #include <QThread>
 
-#include <QMapboxGL>
-
 #include "u2_window.h"
-#include "mapwindow.hpp"
 
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-
-    QMapboxGLSettings settings;
-    settings.setCacheDatabasePath("/tmp/mbgl-cache.db");
-    settings.setCacheDatabaseMaximumSize(20 * 1024 * 1024);
 
     QCommandLineParser parser;
     parser.addHelpOption();
@@ -23,14 +16,8 @@ int main(int argc, char **argv)
 
     U2Window u2window;
     
-    MapWindow mapwindow(settings);
-    
     u2window.resize(800, 600);
     u2window.show();
-
-    mapwindow.resize(800, 600);
-    mapwindow.show();
-
 
     return app.exec();
     
