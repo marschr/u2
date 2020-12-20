a297cbd1ac30462218ab9c7f7bf306377daccb98
Marcos Scheeren <marcosscheeren@gmail.com>
2020-12-20 08:10:31 -0800

    QT OGL boilerplate

--- u2_window.cpp
+++ u2_window.cpp
@@ -1,21 +1,40 @@
 #include "u2_window.h"
 #include <QDebug>
+#include <QVBoxLayout>
 
 #include "location.h"
+
+
+GLWindow::GLWindow(QWidget *parent) : QOpenGLWidget(parent) {
+    qDebug() << "GLWindow ctor";
+
+}
+GLWindow::~GLWindow() {
+    qDebug() << "GLWindow dtor";
+}
 
 U2Window::U2Window()
     : location(nullptr)
 {
-    qDebug() << "ctor";
+    qDebug() << "U2Window ctor";
 
+
+    QVBoxLayout *main_layout = new QVBoxLayout();
+    main_layout->setMargin(0);
+
+    glWindow = new GLWindow(this);
+    main_layout->addWidget(glWindow, 0, 0);
+
+    //location stuff
     QThread *thread = new QThread;
     location = new Location();
     location->moveToThread(thread);
-
     connect(thread, &QThread::started, location, &Location::handle_message);
     connect(location, SIGNAL (newMsg()), this, SLOT (recvMsg()));
-
     thread->start();
+
+    setLayout(main_layout);
+    setStyleSheet(R"(background-color: red;)");
 }
 
 void U2Window::recvMsg(){
@@ -23,5 +42,5 @@ void U2Window::recvMsg(){
 }
 
 U2Window::~U2Window(){
-    qDebug() << "dtor";
+    qDebug() << "U2Window dtor";
 }
--- u2_window.h
+++ u2_window.h
@@ -1,15 +1,30 @@
-#include <QWindow>
+#include <QWidget>
 
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions>
 
 #include "location.h"
 
-class U2Window : public QWindow
+// container window for onroad NVG UI
+class GLWindow : public QOpenGLWidget, protected QOpenGLFunctions {
+  Q_OBJECT
+
+    public:
+        using QOpenGLWidget::QOpenGLWidget;
+        explicit GLWindow(QWidget *parent = 0);
+        void wake();
+        ~GLWindow();
+};
+
+class U2Window : public QWidget
 {
     Q_OBJECT
 
     public:
         U2Window();
         ~U2Window();
+
+        GLWindow *glWindow;
 
     private slots:
         void recvMsg();
